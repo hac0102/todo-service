9d9c0d78d8472f9915e28b61cc290f76b4fa3025
haChangJin <hac198912@gmail.com>
2020-06-18 01:10:38 +0900

    - springSecurity 설정

---
+++ src/main/java/com/hac/todo/config/auth/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.hac.todo.config.auth;
+
+import com.hac.todo.web.dto.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/", "/css/**", "/img/**", "/js/**", "/profile").permitAll()
+            .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+            .anyRequest().authenticated()
+        .and()
+            .logout()
+            .logoutSuccessUrl("/")
+        .and()
+            .oauth2Login()
+            .userInfoEndpoint()
+            .userService(customOAuth2UserService);
+    }
+}
