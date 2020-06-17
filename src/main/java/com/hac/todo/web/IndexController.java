d3782c662d5e52444bc85ce1935f17d8eaa05513
haChangJin <hac198912@gmail.com>
2020-06-18 01:13:01 +0900

    - 세션 공통으로 뻄

--- src/main/java/com/hac/todo/web/IndexController.java
+++ src/main/java/com/hac/todo/web/IndexController.java
@@ -1,4 +1,37 @@
 package com.hac.todo.web;
 
+import com.hac.todo.config.auth.LoginUser;
+import com.hac.todo.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
 public class IndexController {
+
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView mv, @LoginUser SessionUser user) throws Exception {
+//        log.info("user name :: {}", user.getName());
+//        log.info("user email :: {}", user.getEmail());
+//        log.info("user picture :: {}", user.getPicture());
+
+        if(user == null){
+            mv.setViewName("login");
+            return mv;
+        }
+
+
+        mv.addObject("userInfo", user);
+        mv.setViewName("main");
+
+        return mv;
+    }
 }
